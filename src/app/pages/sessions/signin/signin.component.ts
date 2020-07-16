48171ef9c3f2bf6a2b5d46a3bf3c22cba4d2c737
Santu Nandi <santunandi.nandi@gmail.com>
2020-07-16 23:14:57 +0530

    Login page input binding

--- src/app/pages/sessions/signin/signin.component.ts
+++ src/app/pages/sessions/signin/signin.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from '@env/environment';
+import { APIService } from '@shared/services';
 
 
 @Component({
@@ -10,17 +11,37 @@ import { environment } from '@env/environment';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
- 
-  constructor( private router: Router) {
-
-  }
+  loginForm: FormGroup;
+  constructor(
+    private router: Router,
+    private _fb: FormBuilder,
+    private _apiService: APIService
+  ) { }
 
   ngOnInit() {
+    this.userForm();
+  }
 
+  userForm() {
+    this.loginForm = this._fb.group({
+      userName: ["", [Validators.required]],
+      password: ["", [Validators.required]]
+    });
+  }
+
+  login() {
+    console.log("data==", this.loginForm);
+    const reqData = this.loginForm.value;
+    // this._apiService.ApiCall(reqData, `${environment.apiUrl}signin`, 'post').subscribe(result => {
+    //     if (result.success) {
+        this.router.navigate(["dashboard"]);
+    //     }
+    //   }, err => {   }
+    // );
+  }
+
+  ngOnDestroy() {
 
   }
-  ngOnDestroy(){
-    
-  }
-  
+
 }
