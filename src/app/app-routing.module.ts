40adaab5fbd767abda52f0933e1b6972cb95ff70
chandra-joldao <cmanna7@gmail.com>
2020-07-16 21:01:25 +0530

    testdfg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { AdminLayoutComponent, AuthLayoutComponent } from '@shared/components';
 import { AuthGuard } from '@shared/services/auth';
@@ -30,7 +30,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false, preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
