8d74ec211439bd675bbdd714793d045b6acf14f6
chandra-joldao <cmanna7@gmail.com>
2020-07-26 09:22:33 +0530

    changes

--- src/app/shared/components/app-admin-top-panel/app-admin-top-panel.component.ts
+++ src/app/shared/components/app-admin-top-panel/app-admin-top-panel.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SideNavService } from '@shared/services';
 import { Location } from '@angular/common';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-app-admin-top-panel',
@@ -8,10 +10,18 @@ import { Location } from '@angular/common';
   styleUrls: ['./app-admin-top-panel.component.scss']
 })
 export class AppAdminTopPanelComponent implements OnInit {
-
+  url;
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
     private _location: Location,
-    private toolbarService: SideNavService) { }
+    private toolbarService: SideNavService) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.url = this.router.url.split('/');
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
