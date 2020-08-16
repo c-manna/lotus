03a91fe74af74a94aea5d42787dade04c5afa342
chandra-joldao <cmanna7@gmail.com>
2020-08-16 14:00:59 +0530

    change

--- src/app/shared/components/admin-rightside-panel/admin-rightside-panel.component.ts
+++ src/app/shared/components/admin-rightside-panel/admin-rightside-panel.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { APIService, DataService, SideNavService } from '@shared/services';
 import { CookieService } from 'ngx-cookie-service';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-admin-rightside-panel',
@@ -10,6 +11,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class AdminRightsidePanelComponent implements OnInit {
   user:any={};
+  userBalance:any={};
 
   constructor(private ds: DataService,
     private apiService: APIService,
@@ -32,5 +34,17 @@ export class AdminRightsidePanelComponent implements OnInit {
     this.closeNav();
     this._cookieService.deleteAll();
     this._router.navigate(["/"]);
+  }
+  getSettingData() {
+    let param:any={};
+    param.user_id=this.user.punter_id;
+    this.apiService.ApiCall(param, `${environment.apiUrl}setting`, 'get').subscribe(res => {
+      if (res.success) {
+        this.userBalance=res.data;
+        //this.ds.changeSettingData(res.data);
+      } else {
+      }
+    }, err => {
+    });
   }
 }
