663933f72c1a3442fb3a5f602112ca0ad3255e9a
Santu Nandi <santunandi.nandi@gmail.com>
2020-09-01 00:01:05 +0530

    Add the open bet page redirection

--- src/app/pages/open-bets/open-bets.component.ts
+++ src/app/pages/open-bets/open-bets.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SnakebarService, LoadingService } from '@app/shared/services/common.service';
 import { environment } from '@env/environment';
 import { APIService } from '@app/shared/services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-open-bets',
@@ -14,16 +15,19 @@ export class OpenBetsComponent implements OnInit {
   matchBet: any = [];
   unmatchBet: any = [];
   openBetInterval: any;
+  events = [{ "eventType": "1", "name": "Soccer", "marketCount": 2492 }, { "eventType": "2", "name": "Tennis", "marketCount": 5578 }, { "eventType": "4", "name": "Cricket", "marketCount": 22 }, { "eventType": "7", "name": "Horse Racing", "marketCount": 831 }, { "eventType": "4339", "name": "Greyhound Racing", "marketCount": 298 }];
   loading: boolean = true;
   constructor(
     private apiService: APIService,
     private _loadingService: LoadingService,
-    private _snakebarService: SnakebarService
+    private _snakebarService: SnakebarService,
+    private _router: Router
   ) { }
 
   ngOnInit(): void {
     this.getOpenBets();
     this.getOpenBetFromInterval();
+    this.getEvents();
   }
 
   getOpenBets() {
@@ -60,6 +64,23 @@ export class OpenBetsComponent implements OnInit {
 
   ngOnDestroy() {
     clearInterval(this.openBetInterval);
+  }
+
+  getEvents() {
+    this.apiService.ApiCall('', environment.apiUrl + 'event', 'get').subscribe(result => {
+      if (result.success) {
+        this.events = result.data;
+      }
+    }, err => {
+
+    });
+  }
+
+  goToDetailsPage(betData) {
+    let event = this.events.find(item => item.name === betData.event_name);
+    // if (event) { //matches/:competitionId/match-details/:matchId
+    //   this._router.navigate([`/series/${event.eventType}/matches/${betData.competition_id}/match-details/${betData.match_id}`]);
+    // }
   }
 
 }
