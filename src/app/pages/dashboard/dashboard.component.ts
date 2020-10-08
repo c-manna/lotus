e1ee3b6094abce3816b58f2e9ccfcf18aa06abf3
chandra-joldao <cmanna7@gmail.com>
2020-10-08 23:10:41 +0530

    changes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -175,33 +175,6 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  /* openCreateBetForm(value, type, item, index, fragment, eachMatch, dataList_i, dataList_j, event_name) {
-    this.current_exposure = [];
-    console.log(eachMatch)
-    this.details.marketId = eachMatch.runner_details.marketId;
-    this.details.market_start_time = eachMatch.runner_details.marketStartTime;
-    this.details.runners = eachMatch.runner_details;
-    this.details.index = index;
-    this.details.fragment = fragment;
-    this.details.market_type = eachMatch.runner_details.marketName;
-    this.details.runnerName = eachMatch.runner_details[fragment].runnerName;
-    this.details.event_id = eachMatch.event_id;
-    this.maxBetMaxMarketFinal = [];
-    this.maxBetMaxMarketFinal = this.maxBetMaxMarket.find(obj => obj.event == eachMatch.event_id) == undefined ? { status: false } : this.maxBetMaxMarket.find(obj => obj.event == eachMatch.event_id);
-    this.details.event_name = event_name;
-    this.details.description = eachMatch.match_name;
-    this.details.competition_id = eachMatch.competetion_id;
-    this.details.match_id = eachMatch.match_id;
-    let currentTime = new Date().getTime();
-    this.currentTime = currentTime;
-    eachMatch.currentTime = currentTime;
-    this.selectedItem = { type: type, ...item, value: value, dataList_i: dataList_i, dataList_j: dataList_j };
-    this.current_exposure = [];
-    this.details.runners.forEach(element => {
-      this.current_exposure.push("0.00");
-    });
-  } */
-
   showMatchName(matchName, team) {
     let index = matchName.indexOf(" v ")
     if (team == 1) {
